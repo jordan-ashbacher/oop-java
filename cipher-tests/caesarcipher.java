cc4a81ee5cb80cb1285263da8e9c970660ead5cc
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-10 16:39:02 -0500

    creates cipher algorithm to encrypt input

---
+++ cipher-tests/caesarcipher.java
@@ -0,0 +1,23 @@
+public class caesarcipher {
+    public String encrypt(String input, int key) {
+
+        StringBuilder encrypted = new StringBuilder(input);
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(currChar);
+
+            if (idx != -1) {
+                char newChar = shiftedAlphabet.charAt(idx);
+                encrypted.setCharAt(i, newChar);
+            };
+
+        };
+        return encrypted.toString();
+        
+    };
+}
